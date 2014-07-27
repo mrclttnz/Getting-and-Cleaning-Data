4cb2412bc0c63edbe4c9ee306c48130dae4c4cea
Marco Lattanzio <marco.lattanzio@gmail.com>
2014-07-27 16:38:29 +0200

    added more comments to R code

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,5 @@
 
-setwd("C:/projects/Coursera/Getting and Cleaning Data/Assignment/work/Getting-and-Cleaning-Data")
+setwd("C:/projects/Coursera/Getting and Cleaning Data/Assignment/work/Getting-and-Cleaning-Data")  # sets the working directory
 
 library(plyr)
 
@@ -23,11 +23,11 @@ y_train = read.table(file="./indata/y_train.txt", sep="", header=F, col.names=c(
 
 #### Step 1: Merges the training and the test sets to create one data set.
 
-# combines training and test for X data
+# combines training and test for X data (the measurements of the 561 features for each of the recordings)
 xData = rbind(x_train, x_test)
 head(xData)
 
-# combines training and test for subject IDs
+# combines training and test for subject IDs 
 subjectIDs = rbind(subject_train, subject_test)
 head(subjectIDs)
 
@@ -35,7 +35,9 @@ head(subjectIDs)
 activityIDs = rbind(y_train, y_test)
 head(activityIDs)
 
-# create first data set with all information (relies on no one having changed order.... better explain!!!)
+# create first data set with all information 
+#    since we have kept the same row order of the input files (no operation involving reordering so far), 
+#    we can simply use cbind to combine the three data frames
 data = cbind(xData, subjectIDs, activityIDs)
 
 
@@ -44,11 +46,13 @@ data = cbind(xData, subjectIDs, activityIDs)
 # select only the columns whose name contains "mean()" or "std()" in the feature description file (features.txt)
 # also include the last two columns (562 and 563) that have been added to the dataset (subject.ID and activity.ID) 
 
+# using regular expressions to subset features and keep only the ones who have "mean()" or "std()" in their name
 selFeatureIdx = grep("(mean|std)[(][)]", features$feature.name, ignore.case=T)
 selectedFeatures = features[selFeatureIdx,]
-# selectedFeatures = features[c(1,2,4,5),]   ## use this only while testing the code to be able to manage the number of variables to print on screen
+# selectedFeatures = features[c(1,2,4,5),]   ## use this only while testing the code to be able to manage the number of variables to print on screen and pretending that only 4 features are selected
 selectedFeatures
 
+# creates a new data frame with only the features that have been requested, plus add subject.ID and activity.ID
 selectedColumns = data[, c(selectedFeatures$feature.ID,562,563)]
 #selectedColumns[1:10,]
 
@@ -67,16 +71,22 @@ allData = join(selectedColumns, activity_labels, by="activity.ID", match="first"
 ## forces "subject.ID", "activity.ID" and "activity.name" for the three other variables
 colnames(allData) = c("activity.ID", as.character(selectedFeatures$feature.name), "subject.ID", "activity.name")
 #allData[1:10,]
+## reorder the columns to have subject.ID, activity.ID and activity.name as the first three columns and then all features
 allData = allData[,c(ncol(allData)-1, 1, ncol(allData),seq(2,ncol(allData)-2))]
 #allData[1:10,]
 
 
 #### Step 5: Creates a second, independent tidy data set with the average of each variable for each activity and each subject
 
+# uses aggregate function (from plyr package) to aggregate the data and calculate the averages by activity and subject
 averages = aggregate(. ~ activity.ID + activity.name  + subject.ID, allData, mean)
+# orders the final dataset by activity.ID, activity.name and subject.ID
 averages = arrange(averages, activity.ID, activity.name, subject.ID)
+# add "Average.of." in front of the names for the averages that have just been calculated
 names(averages) = c(names(averages[,1:3]),paste("Average.of.",names(averages[,4:ncol(averages)]),sep=""))
 averages[1:10,]
 
+# write to the output text files: allData.txt would not be required but it's useful to QA the results
+# averages.txt is our final independent tidy dataset
 write.table(allData, file="./outdata/allData.txt", sep="\t", row.names=F)
 write.table(averages, file="./outdata/averages.txt", sep="\t", row.names=F)
